d3592940aace848ccd0c7d86b3f4f130fdfa1de0
Brandon Holtsclaw <me@brandonholtsclaw.com>
2012-04-21 12:44:28 -0500

    adding --HEAD option to install latest bzr push version

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -3,11 +3,14 @@ require 'formula'
 class Juju < Formula
   skip_clean :all
   url 'https://launchpad.net/ubuntu/precise/+source/juju/0.5+bzr531-0ubuntu1/+files/juju_0.5+bzr531.orig.tar.gz'
-  #HEAD 'lp:juju', :using => :bzr
+  head 'lp:juju', :using => :bzr
   homepage 'https://launchpad.net/ubuntu/+source/juju'
   md5 '09be4a473644f3c88f64c3e29491c9af'
-  version '0.5-531'
+  version '0.5.531'
   
+  depends_on 'bzr'
+  depends_on 'zookeeper'
+
   def install
     ENV.deparallelize
     ENV.no_optimization
